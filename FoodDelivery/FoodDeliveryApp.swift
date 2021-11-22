9bdac2d3bfce91f616581e7b68a286c8594b435a
Alexandr Arsenuk <alexandr.arsenuk@devexperts.com>
2021-11-22 19:01:28 +0300

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Александр Арсенюк on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
